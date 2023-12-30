6611ceb01356b8ca4e3a168119a9e54a1ceaddc1
osama-mustafa <semsem6661@gmail.com>
2023-12-30 22:00:50 +0200

    Feature: Hash Passwords

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -25,5 +26,24 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+
+// Hash password before save
+UserSchema.pre('save', async function (next) {
+    let user = this;
+    if (user.isModified('password')) {
+        let salt = await bcrypt.genSalt();
+        await bcrypt.hash(this.password, salt, function (err, hash) {
+            if (err) {
+                return next(err);
+            } else {
+                user.password = hash;
+                next();
+            }
+        });
+    } else {
+        next();
+    }
+})
 
 module.exports = mongoose.model('User', UserSchema);
