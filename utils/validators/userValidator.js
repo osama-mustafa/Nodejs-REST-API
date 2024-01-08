bd25e41f479b572c42799df294257586975ac5f8
osama-mustafa <semsem6661@gmail.com>
2024-01-08 16:00:48 +0200

    ADD: Add Create-User-Validator in Registeration

--- utils/validators/userValidator.js
+++ utils/validators/userValidator.js
@@ -13,6 +13,14 @@ const createUserValidator = async (req, res, next) => {
         .withMessage('Please provide a valid email address')
         .escape()
         .run(req);
+    await body('role')
+        .optional()
+        .isAlpha()
+        .run(req);
+    await body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .run(req);
 
     await respondWithValidationErrors(req, res, next);
 }
